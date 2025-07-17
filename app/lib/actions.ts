8f9b10be77eb1cc0841718c73abea99663dff4e7
Topgyal Gurung <topgyaltsering3@gmail.com>
2025-07-16 23:23:57 -0400

    feat:complete updating and deleting invoice with dynamic route

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -15,7 +15,9 @@ const FormSchema = z.object({
     date:z.string(),
 })
 
+// use zod to update expected types
 const CreateInvoice = FormSchema.omit({id:true, date:true});
+const UpdateInvoice = FormSchema.omit({id:true, date:true});
 
 export async function createInvoice(formData: FormData){
     const {customerId, amount, status} = CreateInvoice.parse({
@@ -38,4 +40,29 @@ export async function createInvoice(formData: FormData){
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 
+}
+
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices'); // clear cache and make new server request
+  redirect('/dashboard/invoices'); // redirect to invoices page
+}
+
+export async function deleteInvoice(id:string){
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices'); // clear cache and make new server request
 }
