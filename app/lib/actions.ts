b60187e45d21b8b10a55147cb4e423fc9af8b471
Topgyal Gurung <topgyaltsering3@gmail.com>
2025-07-18 18:23:52 -0400

    feat: completed server side form validation, useActionState, handle and show form erros

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import {z} from 'zod';
+import {string, z} from 'zod';
 import { revalidatePath } from 'next/cache';
 import {redirect} from 'next/navigation';
 import postgres from 'postgres';
@@ -9,23 +9,48 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
     id:z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status:z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: "Please select a customer",
+    }),
+    amount: z.coerce.number()
+        .gt(0, {message: "Please enter an amount greater than 0"}),
+    status:z.enum(['pending', 'paid'],{
+        invalid_type_error: "Please select an invoice status",
+    }),
     date:z.string(),
 })
+
+export type State ={
+    errors?:{
+        customerId?:string[];
+        amount?:string[];
+        status?:string[];
+    };
+    message?:string | null;
+}
 
 // use zod to update expected types
 const CreateInvoice = FormSchema.omit({id:true, date:true});
 const UpdateInvoice = FormSchema.omit({id:true, date:true});
 
-export async function createInvoice(formData: FormData){
-
-    const {customerId, amount, status} = CreateInvoice.parse({
+export async function createInvoice( prevState: State, formData: FormData){
+    // safeParse return object success or error, will help handle more gracefully without try catch block
+    const validateFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount:formData.get('amount'),
         status:formData.get('status'),
     });
+
+    if(!validateFields.success){
+        return {
+            errors:validateFields.error.flatten().fieldErrors,
+            message: 'Missing fields. Failed to create invoice.',
+        }
+    }
+
+    // prepare data for insertion into db
+    const {customerId, amount, status} = validateFields.data;
+
     // It's usually good practice to store monetary values in cents in your database to eliminate JavaScript floating-point errors and ensure greater accuracy.
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0]; // Get the current date in YYYY-MM-DD format
@@ -48,26 +73,36 @@ export async function createInvoice(formData: FormData){
 }
 
  
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState:State, formData: FormData) {
+
+    const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
  
-  const amountInCents = amount * 100;
- try{
-    await sql`
-        UPDATE invoices
-        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-        WHERE id = ${id}
-    `;
- }catch(error){
-    console.error(error);
- }
- 
-  revalidatePath('/dashboard/invoices'); // clear cache and make new server request
-  redirect('/dashboard/invoices'); // redirect to invoices page
+    if(!validatedFields.success){
+        return{
+            errors:validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to updated invoice',
+        }
+    }
+
+    const {customerId, amount, status} = validatedFields.data;
+
+    const amountInCents = amount * 100;
+    try{
+        await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+            WHERE id = ${id}
+        `;
+    }catch(error){
+        return{message: "Database Error: Failed to Update Invoice"}
+    }
+    
+    revalidatePath('/dashboard/invoices'); // clear cache and make new server request
+    redirect('/dashboard/invoices'); // redirect to invoices page
 }
 
 export async function deleteInvoice(id:string){
